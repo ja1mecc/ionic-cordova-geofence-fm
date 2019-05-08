731286bdd24ea648704cc4d6e857cf95e0fa6c4e
ja1mecc <49685614+ja1mecc@users.noreply.github.com>
2019-05-08 09:16:30 -0400

    up

--- src/ios/GeofenceFM.m
+++ src/ios/GeofenceFM.m
@@ -233,13 +233,4 @@
     
 }
 
-- (void)getScreenSize:(CDVInvokedUrlCommand*)command
-{
-    NSLog(@"getScreenSize -> GeofenceFM");
-
-    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
 @end
